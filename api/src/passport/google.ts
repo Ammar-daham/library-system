7c1bc53b260c4ae13fcb5ab938e770059d08c452
Ammar Daham <ammar.daham@hva.nl>
2022-09-29 16:23:57 +0300

    post and verify isAdmin user or admin implemented

--- api/src/passport/google.ts
+++ api/src/passport/google.ts
@@ -18,13 +18,23 @@ export default function () {
         console.log('parsedToken: ', parsedToken)
         let user: any = await User.findOne({ email: parsedToken.payload.email })
         if (!user) {
-          user = new User({
-            username: parsedToken.payload.name,
-            firstname: parsedToken.payload.given_name,
-            lastname: parsedToken.payload.family_name,
-            email: parsedToken.payload.email,
-            isAdmin: false,
-          })
+          if (parsedToken.payload.email === 'ammar.daham@integrify.io') {
+            user = new User({
+              username: parsedToken.payload.name,
+              firstname: parsedToken.payload.given_name,
+              lastname: parsedToken.payload.family_name,
+              email: parsedToken.payload.email,
+              isAdmin: true,
+            })
+          } else {
+            user = new User({
+              username: parsedToken.payload.name,
+              firstname: parsedToken.payload.given_name,
+              lastname: parsedToken.payload.family_name,
+              email: parsedToken.payload.email,
+              isAdmin: false,
+            })
+          }
           user.save()
         }
         done(null, user)
